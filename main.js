11cbb8c3267c82993e43c0a342304d7d98c67d5e
untitledV01D <miguel126xl9@gmail.com>
2023-06-14 18:20:21 -0500

    Carrito compras e interfaz

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const BurguerMenuIcon = document.querySelector('.menu');
+const burguerMenuIcon = document.querySelector('.menu');
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(){
+    const isProductDetailOpen = !productDetail.classList.contains('inactive');
+
+    if(isProductDetailOpen){
+        productDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    console.log('Click');
+    const isProductDetailOpen = !productDetail.classList.contains('inactive');
+
+    if(isProductDetailOpen){
+        productDetail.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCartMenu(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add('inactive');
+    }
+    
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add('inactive');
+    }
+
+    productDetail.classList.toggle('inactive');
+}
+
 navEmail.addEventListener('click', toggleDesktopMenu);
-BurguerMenuIcon.addEventListener('click', toggleMobileMenu);
+burguerMenuIcon.addEventListener('click', toggleMobileMenu);
+cartMenuIcon.addEventListener('click', toggleCartMenu);
+
 
