3470334ea77264ef5acf6094d407a9f3ba6947b3
untitledV01D <miguel126xl9@gmail.com>
2023-06-14 16:42:14 -0500

    Fusion menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const BurguerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-
 }
 
-navEmail.addEventListener('click', toggleDesktopMenu)
+function toggleMobileMenu(){
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
+}
+
+navEmail.addEventListener('click', toggleDesktopMenu);
+BurguerMenuIcon.addEventListener('click', toggleMobileMenu);
 
